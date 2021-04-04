632cb08761b3b8e1bbf49894820e12c73cdc3215
carlosmonzon2008 <carlosmonzon2008@gmail.com>
2021-04-04 19:28:50 +0200

    Update main.java

--- src/main/java/demo/rest/main.java
+++ src/main/java/demo/rest/main.java
@@ -13,6 +13,8 @@ public class Demo_Main {
     @GET
     @Produces("application/json")
     public String demo_main(){
-        return {"response": "hello!"};
+        JSONParser parser = new JSONParser();
+        JSONObject json_response = (JSONObject) parser.parse('{"response": "hello!"}');
+        return json_response;
     }
 }
