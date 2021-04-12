3a4be6e2cc7cb17b248c21c0d84e61d5e0b3d3bd
carlosmonzon2008 <carlosmonzon2008@gmail.com>
2021-04-12 15:37:06 +0200

    Update Demo_Main.java

--- src/main/java/demo/rest/Demo_Main.java
+++ src/main/java/demo/rest/Demo_Main.java
@@ -3,6 +3,7 @@ package demo.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import io.vertx.core.json.JsonObject;
 
 /**
  * Created by Carlos Monzón on 04/April/2021.
@@ -13,6 +14,15 @@ public class Demo_Main {
 
     @RequestMapping(method = RequestMethod.GET)
     public String response() {
-        return "{'response': 'hello 12:07'}";
+        private static final Integer MESSAGE_COUNT = 1;
+        private static final String GRETTING_MESSAGE = "Hola";
+        private static final String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknown");
+        private static final String BACKGROUND_COLOR = "#FF5733";
+        JsonObject response = new JsonObject()
+                            .put("msgCount", MESSAGE_COUNT)
+                            .put("greettingMessage", GRETTING_MESSAGE)
+                            .put("podName", HOSTNAME)
+                            .put("backgroundColor", BACKGROUND_COLOR);
+        return response.encode();
     }
 }
