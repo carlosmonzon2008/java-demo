166bdfa492601dca1d66a34bbe5e8f98b842c6a9
carlosmonzon2008 <carlosmonzon2008@gmail.com>
2021-04-12 17:39:31 +0200

    Update Demo_Main.java

--- src/main/java/demo/rest/Demo_Main.java
+++ src/main/java/demo/rest/Demo_Main.java
@@ -16,6 +16,9 @@ public class Demo_Main {
     private String GRETTING_MESSAGE = "Hola.";
     private String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknown");
     private String BACKGROUND_COLOR = "#5bbf45";
+    //private String BACKGROUND_COLOR = "#6bbded";
+    //private String BACKGROUND_COLOR = "#f2f25e";
+    //private String BACKGROUND_COLOR = "#FF5733";
 
     @RequestMapping(method = RequestMethod.GET)
     public String response() {
