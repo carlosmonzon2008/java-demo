2c08a6afeaee644caae9807ffb8947548a68bf21
carlosmonzon2008 <carlosmonzon2008@gmail.com>
2021-04-12 16:21:51 +0200

    Update Demo_Main.java

--- src/main/java/demo/rest/Demo_Main.java
+++ src/main/java/demo/rest/Demo_Main.java
@@ -12,12 +12,13 @@ import io.vertx.core.json.JsonObject;
 @RequestMapping("/api")
 public class Demo_Main {
 
+    private int MESSAGE_COUNT = 1;
+    private String GRETTING_MESSAGE = "Hola.";
+    //private String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknown");
+    private String BACKGROUND_COLOR = "#FF5733";
+
     @RequestMapping(method = RequestMethod.GET)
     public String response() {
-        private String MESSAGE_COUNT = "1";
-        private String GRETTING_MESSAGE = "Hola.";
-        //private String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknown");
-        private String BACKGROUND_COLOR = "#FF5733";
         JsonObject response = new JsonObject()
                             .put("msgCount", MESSAGE_COUNT)
                             .put("greettingMessage", GRETTING_MESSAGE)
