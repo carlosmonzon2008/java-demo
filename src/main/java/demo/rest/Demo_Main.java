ea1a1ed7b7f5a302592798ea27840b1933d95128
carlosmonzon2008 <carlosmonzon2008@gmail.com>
2021-04-12 17:04:45 +0200

    Update Demo_Main.java

--- src/main/java/demo/rest/Demo_Main.java
+++ src/main/java/demo/rest/Demo_Main.java
@@ -14,7 +14,7 @@ public class Demo_Main {
 
     private int MESSAGE_COUNT = 1;
     private String GRETTING_MESSAGE = "Hola.";
-    //private String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknown");
+    private String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknown");
     private String BACKGROUND_COLOR = "#FF5733";
 
     @RequestMapping(method = RequestMethod.GET)
@@ -22,7 +22,7 @@ public class Demo_Main {
         JsonObject response = new JsonObject()
                             .put("msgCount", MESSAGE_COUNT)
                             .put("greettingMessage", GRETTING_MESSAGE)
-                            //.put("podName", HOSTNAME)
+                            .put("podName", HOSTNAME)
                             .put("backgroundColor", BACKGROUND_COLOR);
         return response.encode();
     }
