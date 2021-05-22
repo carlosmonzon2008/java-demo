828d7a071e022923bfa97db8ce71661089e76643
carlosmonzon2008 <carlosmonzon2008@gmail.com>
2021-05-22 14:52:39 +0200

    Update Demo_Main.java

--- src/main/java/demo/rest/Demo_Main.java
+++ src/main/java/demo/rest/Demo_Main.java
@@ -24,7 +24,8 @@ public class Demo_Main {
     //private String BACKGROUND_COLOR = "#FF5733";//Orange
     //private String BACKGROUND_COLOR = "#FF0000";//Red
     //private String BACKGROUND_COLOR = "Silver";//Silver
-    private String BACKGROUND_COLOR = "Aqua";//Aqua
+    //private String BACKGROUND_COLOR = "Aqua";//Aqua
+    private String BACKGROUND_COLOR = "Bisque";//Bisque
 
     @RequestMapping(method = RequestMethod.GET, produces={"application/json"})
     //@GetMapping(produces = MediaType.APPLICATION_JSON)
